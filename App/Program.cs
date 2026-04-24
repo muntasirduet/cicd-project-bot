c2f278f795caa5eaf65bcd5f2452b1b35ef33ab3
muntasirduet <mdakashbiswas1997@gmail.com>
2026-04-24 14:58:56 +0600

    test: trigger CI/CD pipeline

--- App/Program.cs
+++ App/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Deployed World from WSL!");
+Console.WriteLine("Test deploy at Fri Apr 24 14:58:35 +06 2026");
